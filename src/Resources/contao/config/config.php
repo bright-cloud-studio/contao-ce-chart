1153498df41c509cfa4267c2225d243f53b541ac
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2024-02-21 14:52:16 -0500

    Create config.php

---
+++ src/Resources/contao/config/config.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * @copyright  Bright Cliud Studio
+ * @author     Bright Cloud Studio
+ * @package    Contao CE Chart
+ * @license    LGPL-3.0+
+ * @see	       https://github.com/bright-cloud-studio/contao-ce-chart
+ */
+
+ array_insert($GLOBALS['TL_CTE']['texts'], 10, array
+ (
+ 	'chart' => 'Bcs\ChartBundle\ContentChart')
+ );
+
+?>
