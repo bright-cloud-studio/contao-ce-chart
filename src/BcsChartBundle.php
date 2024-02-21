945ffd4be32bee8d381a65bc8beb4feb7b6890d1
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2024-02-21 14:49:08 -0500

    Create BcsChartBundle.php

---
+++ src/BcsChartBundle.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * @copyright  Bright Cliud Studio
+ * @author     Bright Cloud Studio
+ * @package    Contao CE Hero Text
+ * @license    LGPL-3.0+
+ * @see	       https://github.com/bright-cloud-studio/contao-ce-hero-text
+ */
+
+namespace Bcs\ChartBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class BcsChartBundle extends Bundle
+{
+}
