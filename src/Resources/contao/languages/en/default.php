098761b403c0299d707f3ac35e105b376d15f946
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2024-02-22 11:02:20 -0500

    Update default.php

--- src/Resources/contao/languages/en/default.php
+++ src/Resources/contao/languages/en/default.php
@@ -1,4 +1,12 @@
 <?php
 
+/**
+ * @copyright  Bright Cliud Studio
+ * @author     Bright Cloud Studio
+ * @package    Contao CE Chart
+ * @license    LGPL-3.0+
+ * @see	       https://github.com/bright-cloud-studio/contao-ce-chart
+ */
+
 $GLOBALS['TL_LANG']['CTE']['datasets'] = "Dataset elements";
 $GLOBALS['TL_LANG']['CTE']['chart'][0] = "Line Chart";
