9aec4ea31377eea48bad36f9efe2662ecc316794
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2024-02-22 11:00:26 -0500

    Update BcsChartBundle.php

--- src/BcsChartBundle.php
+++ src/BcsChartBundle.php
@@ -3,9 +3,9 @@
 /**
  * @copyright  Bright Cliud Studio
  * @author     Bright Cloud Studio
- * @package    Contao CE Hero Text
+ * @package    Contao CE Chart
  * @license    LGPL-3.0+
- * @see	       https://github.com/bright-cloud-studio/contao-ce-hero-text
+ * @see	       https://github.com/bright-cloud-studio/contao-ce-chart
  */
 
 namespace Bcs\ChartBundle;
