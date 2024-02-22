6072375402e3c5cc93f6f5a1f074418c30bcfe4c
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2024-02-22 09:58:45 -0500

    Update contao_ce_chart.js

--- src/Resources/public/scripts/contao_ce_chart.js
+++ src/Resources/public/scripts/contao_ce_chart.js
@@ -1,20 +1,10 @@
-const ctx = document.getElementById('myChart');
+var ctx = document.getElementById('myChart'); // node
+var ctx = document.getElementById('myChart').getContext('2d'); // 2d context
+var ctx = $('#myChart'); // jQuery instance
+var ctx = 'myChart'; // element id
 
-new Chart(ctx, {
-type: 'bar',
-data: {
-  labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-  datasets: [{
-    label: '# of Votes',
-    data: [12, 19, 3, 5, 2, 3],
-    borderWidth: 1
-  }]
-},
-options: {
-  scales: {
-    y: {
-      beginAtZero: true
-    }
-  }
-}
+var myChart = new Chart(ctx, {
+  type: 'line',
+  data: {/* Data here */},
+  options: {/* Options here */}
 });
