fae29c659ed8de9a9ecf0ecf0d575633f3a4aef8
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2024-02-22 13:59:48 -0500

    Create ContentBarChart.php

---
+++ src/Resources/contao/elements/ContentBarChart.php
@@ -0,0 +1,46 @@
+<?php
+
+/**
+ * @copyright  Bright Cliud Studio
+ * @author     Bright Cloud Studio
+ * @package    Contao CE Chart
+ * @license    LGPL-3.0+
+ * @see	       https://github.com/bright-cloud-studio/contao-ce-chart
+ */
+
+namespace Bcs\ChartBundle;
+
+class ContentBarChart extends \ContentText
+{
+
+	/**
+	 * Template
+	 * @var string
+	 */
+	protected $strTemplate = 'ce_chart_bar';
+
+
+	/**
+	 * Generate the content element
+	 */
+	public function compile()
+	{
+		parent::compile();
+	}
+
+    /**
+	 * Initialize the object
+	 *
+	 * @param ContentModel $objElement
+	 * @param string       $strColumn
+	 */
+	public function __construct($objElement, $strColumn='main')
+	{
+        parent::__construct($objElement, $strColumn='main');
+        $GLOBALS['TL_JAVASCRIPT']['chart_cdn'] = 'https://cdn.jsdelivr.net/npm/chart.js';
+        $GLOBALS['TL_JAVASCRIPT']['chart_script'] = 'bundles/bcschart/scripts/contao_ce_chart.js';
+	}
+
+
+    
+}
