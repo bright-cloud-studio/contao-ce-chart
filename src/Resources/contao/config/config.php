de141602eab2cf70b3d5bc0d10d577cd3d562ee9
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2024-02-22 14:01:40 -0500

    Update config.php

--- src/Resources/contao/config/config.php
+++ src/Resources/contao/config/config.php
@@ -1,16 +1,17 @@
 <?php
 
-/**
- * @copyright  Bright Cliud Studio
- * @author     Bright Cloud Studio
- * @package    Contao CE Chart
- * @license    LGPL-3.0+
- * @see	       https://github.com/bright-cloud-studio/contao-ce-chart
- */
-
- array_insert($GLOBALS['TL_CTE']['datasets'], 10, array
- (
- 	'chart' => 'Bcs\ChartBundle\ContentChart')
- );
+    /**
+    * @copyright  Bright Cliud Studio
+    * @author     Bright Cloud Studio
+    * @package    Contao CE Chart
+    * @license    LGPL-3.0+
+    * @see	       https://github.com/bright-cloud-studio/contao-ce-chart
+    */
+    
+    array_insert($GLOBALS['TL_CTE']['datasets'], 10, array
+    (
+        'chart_line' => 'Bcs\ChartBundle\ContentLineChart',
+        'chart_bar' => 'Bcs\ChartBundle\ContentBarChart'
+    ));
 
 ?>
