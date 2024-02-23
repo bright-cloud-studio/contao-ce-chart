4f3d5e5db89331421076fbf63aed120db6863cbe
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2024-02-23 14:40:40 -0500

    Update ContentLineChart.php

--- src/Resources/contao/elements/ContentLineChart.php
+++ src/Resources/contao/elements/ContentLineChart.php
@@ -127,7 +127,7 @@ class ContentLineChart extends ContentTable
                                   }
                                 }
                               }
-                            }
+                            },
                 ';
 
             }
