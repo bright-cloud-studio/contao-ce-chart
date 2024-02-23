35b618c250ac7b877d319cc4898a0142d64f16ce
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2024-02-23 14:32:31 -0500

    Update tl_content.php

--- src/Resources/contao/dca/tl_content.php
+++ src/Resources/contao/dca/tl_content.php
@@ -10,16 +10,16 @@
 
 // Get our default 'tl_content' DCA
 $dc = &$GLOBALS['TL_DCA']['tl_content'];
-$GLOBALS['TL_DCA']['tl_content']['palettes']['chart_bar'] = '{type_legend},type,headline;{dataset_legend},tableitems;{chart_config_legend},summary;{template_legend:hide},customTpl;{protected_legend:hide},protected;{expert_legend:hide},guests,cssID;{invisible_legend:hide},invisible,start,stop';
-$GLOBALS['TL_DCA']['tl_content']['palettes']['chart_line'] = '{type_legend},type,headline;{dataset_legend},tableitems;{chart_config_legend},summary;{template_legend:hide},customTpl;{protected_legend:hide},protected;{expert_legend:hide},guests,cssID;{invisible_legend:hide},invisible,start,stop';
+$GLOBALS['TL_DCA']['tl_content']['palettes']['chart_bar'] = '{type_legend},type,headline;{dataset_legend},tableitems;{chart_config_legend},animate;{template_legend:hide},customTpl;{protected_legend:hide},protected;{expert_legend:hide},guests,cssID;{invisible_legend:hide},invisible,start,stop';
+$GLOBALS['TL_DCA']['tl_content']['palettes']['chart_line'] = '{type_legend},type,headline;{dataset_legend},tableitems;{chart_config_legend},animate;{template_legend:hide},customTpl;{protected_legend:hide},protected;{expert_legend:hide},guests,cssID;{invisible_legend:hide},invisible,start,stop';
 
 $arrFields = array(
-    'glide_type'                => array(
-        'label'                    => &$GLOBALS['TL_LANG']['tl_content']['glide_type'],
+    'animate'                => array(
+        'label'                    => &$GLOBALS['TL_LANG']['tl_content']['animate'],
         'inputType'                => 'select',
-		'options'                  => array('slider' => 'Slider', 'carousel' => 'Carousel'),
+		'options'                  => array('yes' => 'Yes', 'no' => 'No'),
 		'eval'                     => array('mandatory'=>true, 'tl_class'=>'w50'),
-		'sql'                      => "varchar(32) NOT NULL default 'slider'"
+		'sql'                      => "varchar(32) NOT NULL default 'yes'"
     ),
 );
 
