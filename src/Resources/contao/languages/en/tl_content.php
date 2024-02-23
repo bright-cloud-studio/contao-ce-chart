a765467c5d328f1509f4f44fd7f66d29f4cfe7f2
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2024-02-23 15:31:36 -0500

    Update tl_content.php

--- src/Resources/contao/languages/en/tl_content.php
+++ src/Resources/contao/languages/en/tl_content.php
@@ -11,7 +11,7 @@
 
 /* Legends */
 $GLOBALS['TL_LANG']['tl_content']['dataset_legend']        = 'Dataset Details';
-$GLOBALS['TL_LANG']['tl_content']['chart_config_legend']   = 'Chart Configuration Settings';
+$GLOBALS['TL_LANG']['tl_content']['chart_line_config_legend']   = 'Chart - Line Configuration';
 
 /* Fields */
 $GLOBALS['TL_LANG']['tl_content']['glide_type']          = array('Glide Type', 'Select the \'type\' setting for Glide');
