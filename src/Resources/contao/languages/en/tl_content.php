2105c10711540a448102401af6cc36fb8beaa9c9
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2024-02-23 14:23:33 -0500

    Create tl_content.php

---
+++ src/Resources/contao/languages/en/tl_content.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * @copyright  Bright Cliud Studio
+ * @author     Bright Cloud Studio
+ * @package    Contao CE Chart
+ * @license    LGPL-3.0+
+ * @see	       https://github.com/bright-cloud-studio/contao-ce-chart
+ */
+  
+
+/* Legends */
+$GLOBALS['TL_LANG']['tl_content']['dataset_legend']    = 'Dataset Details';
+
+/* Fields */
+$GLOBALS['TL_LANG']['tl_content']['glide_type']          = array('Glide Type', 'Select the \'type\' setting for Glide');
