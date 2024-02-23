e6fd479632ac03f544fb9cee7dd6b4ab2e4eb386
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2024-02-23 11:42:13 -0500

    Update ContentLineChart.php

--- src/Resources/contao/elements/ContentLineChart.php
+++ src/Resources/contao/elements/ContentLineChart.php
@@ -27,7 +27,12 @@ class ContentLineChart extends ContentTable
 	 */
 	public function compile()
 	{
-		parent::compile();
+		//$rows = \StringUtil::deserialize($this->tableitems, true);
+		
+		//echo "<pre>";
+		//print_r($rows);
+		//echo "</pre>";
+		//die();
 	}
 
     /**
@@ -38,9 +43,84 @@ class ContentLineChart extends ContentTable
 	 */
 	public function __construct($objElement, $strColumn='main')
 	{
+	    // Run the original construct function
         parent::__construct($objElement, $strColumn='main');
+        
+        
+        // Assemble our table data into usable formats
+        $rows = \StringUtil::deserialize($this->tableitems, true);
+		
+		//echo "<pre>";
+		//print_r($rows);
+		//echo "</pre>";
+		//die();
+        
+        
+        // Include Chart.js and our configuration script
         $GLOBALS['TL_JAVASCRIPT']['chart_cdn'] = 'https://cdn.jsdelivr.net/npm/chart.js';
-        $GLOBALS['TL_JAVASCRIPT']['chart_script'] = 'bundles/bcschart/scripts/contao_ce_chart.js';
+        //$GLOBALS['TL_JAVASCRIPT']['chart_script'] = 'bundles/bcschart/scripts/contao_ce_chart.js';
+        
+        
+        
+        
+        
+        
+        
+        $config = '
+            document.addEventListener("DOMContentLoaded", function () {
+
+                const ctx_'.$this->id.' = document.getElementById("chart_'.$this->id.'")
+            
+                const line_chart__'.$this->id.' = new Chart(ctx_'.$this->id.', {
+                    type: "line",
+                    data: {
+                        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+                        datasets: [
+                            {
+                                label: "# of Votes",
+                                data: [, 19, 3, 5, 2, 3],
+                                backgroundColor: [
+                                    "rgba(255, 99, 132, 0.2)",
+                                    "rgba(54, 162, 235, 0.2)",
+                                    "rgba(255, 206, 86, 0.2)",
+                                    "rgba(75, 192, 192, 0.2)",
+                                    "rgba(153, 102, 255, 0.2)",
+                                    "rgba(255, 159, 64, 0.2)",
+                                ],
+                                borderColor: [
+                                    "rgba(255, 99, 132, 1)",
+                                    "rgba(54, 162, 235, 1)",
+                                    "rgba(255, 206, 86, 1)",
+                                    "rgba(75, 192, 192, 1)",
+                                    "rgba(153, 102, 255, 1)",
+                                    "rgba(255, 159, 64, 1)",
+                                ],
+                                borderWidth: 1,
+                            },
+                        ],
+                    },
+                    options: {
+                        scales: {
+                            y: {
+                                beginAtZero: true,
+                            },
+                        },
+                    },
+                });
+            });
+        ';
+
+        $GLOBALS['TL_BODY'][] = '<script>' . $config . '</script>';
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
 	}   
 }
 
