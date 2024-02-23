85289cb536a196a34eaa7b0ee3b8c7d12d4c16c8
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2024-02-23 14:22:15 -0500

    Update tl_content.php

--- src/Resources/contao/dca/tl_content.php
+++ src/Resources/contao/dca/tl_content.php
@@ -10,5 +10,35 @@
 
 // Get our default 'tl_content' DCA
 $dc = &$GLOBALS['TL_DCA']['tl_content'];
-$GLOBALS['TL_DCA']['tl_content']['palettes']['chart_bar'] = '{type_legend},type,headline;{table_legend},tableitems;{tconfig_legend},summary,thead,tfoot,tleft;{sortable_legend:hide},sortable;{template_legend:hide},customTpl;{protected_legend:hide},protected;{expert_legend:hide},guests,cssID;{invisible_legend:hide},invisible,start,stop';
-$GLOBALS['TL_DCA']['tl_content']['palettes']['chart_line'] = '{type_legend},type,headline;{table_legend},tableitems;{tconfig_legend},summary,thead,tfoot,tleft;{sortable_legend:hide},sortable;{template_legend:hide},customTpl;{protected_legend:hide},protected;{expert_legend:hide},guests,cssID;{invisible_legend:hide},invisible,start,stop';
+$GLOBALS['TL_DCA']['tl_content']['palettes']['chart_bar'] = '{type_legend},type,headline;{dataset_legend},tableitems;{dataset_config_legend},summary;{template_legend:hide},customTpl;{protected_legend:hide},protected;{expert_legend:hide},guests,cssID;{invisible_legend:hide},invisible,start,stop';
+$GLOBALS['TL_DCA']['tl_content']['palettes']['chart_line'] = '{type_legend},type,headline;{dataset_legend},tableitems;{dataset_config_legend},summary;{template_legend:hide},customTpl;{protected_legend:hide},protected;{expert_legend:hide},guests,cssID;{invisible_legend:hide},invisible,start,stop';
+
+$arrFields = array(
+    'glide_type'                => array(
+        'label'                    => &$GLOBALS['TL_LANG']['tl_content']['glide_type'],
+        'inputType'                => 'select',
+		'options'                  => array('slider' => 'Slider', 'carousel' => 'Carousel'),
+		'eval'                     => array('mandatory'=>true, 'tl_class'=>'w50'),
+		'sql'                      => "varchar(32) NOT NULL default 'slider'"
+    ),
+    'starting_slide'            => array(
+        'label'                    => &$GLOBALS['TL_LANG']['tl_content']['starting_slide'],
+        'inputType'                => 'text',
+		'eval'                     => array('tl_class'=>'w50'),
+		'sql'                      => "varchar(12) NOT NULL default ''"
+    ),
+    'slides_to_show'            => array(
+        'label'                    => &$GLOBALS['TL_LANG']['tl_content']['slides_to_show'],
+        'inputType'                => 'text',
+		'eval'                     => array('tl_class'=>'w50'),
+		'sql'                      => "varchar(12) NOT NULL default ''"
+    ),
+    'slide_padding'            => array(
+        'label'                    => &$GLOBALS['TL_LANG']['tl_content']['slide_padding'],
+        'inputType'                => 'text',
+		'eval'                     => array('tl_class'=>'w50'),
+		'sql'                      => "varchar(12) NOT NULL default ''"
+    )
+);
+
+$dc['fields'] = array_merge($dc['fields'], $arrFields);
