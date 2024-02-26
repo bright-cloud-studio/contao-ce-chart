9da24b929f75476ac8241ae45e66df62d6463f24
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2024-02-26 12:07:30 -0500

    Update tl_content.php

--- src/Resources/contao/languages/en/tl_content.php
+++ src/Resources/contao/languages/en/tl_content.php
@@ -14,4 +14,11 @@ $GLOBALS['TL_LANG']['tl_content']['dataset_legend']        = 'Dataset Details';
 $GLOBALS['TL_LANG']['tl_content']['chart_line_config_legend']   = 'Chart - Line Configuration';
 
 /* Fields */
-$GLOBALS['TL_LANG']['tl_content']['glide_type']          = array('Glide Type', 'Select the \'type\' setting for Glide');
+$GLOBALS['TL_LANG']['tl_content']['animate']          = array('Show/Hide Animation', 'Choose if the lines should animate when showing or hiding');
+$GLOBALS['TL_LANG']['tl_content']['line_tension']          = array('Line Tension', 'Lower tension makes lines rigid, higher makes them smoother');
+$GLOBALS['TL_LANG']['tl_content']['line_border_color']          = array('Line Stroke Color', 'HTML color for the stroke color of the lines');
+$GLOBALS['TL_LANG']['tl_content']['line_border_width']          = array('Line Stroke Width', 'Changes the thickness of the line stroke');
+$GLOBALS['TL_LANG']['tl_content']['line_border_dash']          = array('Line Border Dash', 'First number is the width of the dash, second number is the space between the dashes');
+$GLOBALS['TL_LANG']['tl_content']['line_border_joint_style']          = array('Line Stroke Joint Style', 'Choose from different styles for the joint styles');
+$GLOBALS['TL_LANG']['tl_content']['line_background_color']          = array('Line Background Color', 'HTML color for the line background');
+$GLOBALS['TL_LANG']['tl_content']['line_fill']          = array('Line Fill', 'Choose if the lines should fill the background to the bottom of the chart');
