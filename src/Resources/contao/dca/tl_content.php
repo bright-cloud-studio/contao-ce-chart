6c1e531f238a383f4cd250e26757d74d226dfcfe
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2024-03-04 15:50:21 -0500

    Update tl_content.php

--- src/Resources/contao/dca/tl_content.php
+++ src/Resources/contao/dca/tl_content.php
@@ -8,199 +8,149 @@
  * @see	       https://github.com/bright-cloud-studio/contao-ce-chart
  */
 
-namespace Bcs\ChartBundle;
+// Get our default 'tl_content' DCA
+$dc = &$GLOBALS['TL_DCA']['tl_content'];
+$GLOBALS['TL_DCA']['tl_content']['palettes']['chart_line'] = '{type_legend},type,headline;{dataset_legend},tableitems;{chart_line_config_legend},animate,line_tension,line_border_color,line_border_width,line_border_dash,line_border_joint_style,line_background_color,line_fill;{line_colors_legend},line_background_colors;{template_legend:hide},customTpl;{protected_legend:hide},protected;{expert_legend:hide},guests,cssID;{invisible_legend:hide},invisible,start,stop';
+$GLOBALS['TL_DCA']['tl_content']['palettes']['chart_bar'] = '{type_legend},type,headline;{dataset_legend},tableitems;{chart_bar_config_legend},bar_background_color,bar_border_width,bar_border_color,bar_border_skipped,bar_border_radius,bar_inflate_amount,bar_point_style;{template_legend:hide},customTpl;{protected_legend:hide},protected;{expert_legend:hide},guests,cssID;{invisible_legend:hide},invisible,start,stop';
 
-use Contao\ContentTable;
+$arrFields = array(
+    'animate'                  => array(
+        'label'                    => &$GLOBALS['TL_LANG']['tl_content']['animate'],
+        'inputType'                => 'select',
+		'options'                  => array('yes' => 'Yes', 'no' => 'No'),
+		'eval'                     => array('mandatory'=>true, 'tl_class'=>'w50'),
+		'sql'                      => "varchar(32) NOT NULL default 'yes'"
+    ),
+    'line_tension'             => array(
+        'label'                    => &$GLOBALS['TL_LANG']['tl_content']['line_tension'],
+        'inputType'                => 'text',
+		'eval'                     => array('tl_class'=>'w50'),
+		'sql'                      => "varchar(12) NOT NULL default '0'"
+    ),
+    'line_border_color'        => array(
+        'label'                    => &$GLOBALS['TL_LANG']['tl_content']['line_border_color'],
+        'inputType'                => 'text',
+		'eval'                     => array('tl_class'=>'w50'),
+		'sql'                      => "varchar(12) NOT NULL default '#ff00ff'"
+    ),
+    'line_border_width'        => array(
+        'label'                    => &$GLOBALS['TL_LANG']['tl_content']['line_border_width'],
+        'inputType'                => 'text',
+		'eval'                     => array('tl_class'=>'w50'),
+		'sql'                      => "varchar(12) NOT NULL default '3'"
+    ),
+    'line_border_dash'         => array(
+        'label'                    => &$GLOBALS['TL_LANG']['tl_content']['line_border_dash'],
+        'inputType'                => 'text',
+		'eval'                     => array('tl_class'=>'w50'),
+		'sql'                      => "varchar(12) NOT NULL default '[0, 0]'"
+    ),
+    'line_border_joint_style'  => array(
+        'label'                    => &$GLOBALS['TL_LANG']['tl_content']['line_border_joint_style'],
+        'inputType'                => 'select',
+		'options'                  => array('round' => 'Round', 'bevel' => 'Bevel', 'miter' => 'Miter'),
+		'eval'                     => array('mandatory'=>true, 'tl_class'=>'w50'),
+		'sql'                      => "varchar(32) NOT NULL default 'round'"
+    ),
+    'line_background_color'    => array(
+        'label'                    => &$GLOBALS['TL_LANG']['tl_content']['line_background_color'],
+        'inputType'                => 'text',
+		'eval'                     => array('tl_class'=>'w50'),
+		'sql'                      => "varchar(12) NOT NULL default '#ff00ff'"
+    ),
+    'line_fill'                => array(
+        'label'                    => &$GLOBALS['TL_LANG']['tl_content']['line_fill'],
+        'inputType'                => 'select',
+		'options'                  => array('false' => 'False', 'true' => 'True'),
+		'eval'                     => array('mandatory'=>true, 'tl_class'=>'w50'),
+		'sql'                      => "varchar(32) NOT NULL default 'false'"
+    ),
 
-class ContentLineChart extends ContentTable
-{
 
-	/**
-	 * Template
-	 * @var string
-	 */
-	protected $strTemplate = 'ce_chart_line';
+    'bar_background_color'        => array(
+        'label'                    => &$GLOBALS['TL_LANG']['tl_content']['bar_background_color'],
+        'inputType'                => 'text',
+		'eval'                     => array('tl_class'=>'w50'),
+		'sql'                      => "varchar(12) NOT NULL default '#ff00ff'"
+    ),
+    'bar_border_width'        => array(
+        'label'                    => &$GLOBALS['TL_LANG']['tl_content']['bar_border_width'],
+        'inputType'                => 'text',
+		'eval'                     => array('tl_class'=>'w50'),
+		'sql'                      => "varchar(12) NOT NULL default '0'"
+    ),
+    'bar_border_color'        => array(
+        'label'                    => &$GLOBALS['TL_LANG']['tl_content']['bar_border_color'],
+        'inputType'                => 'text',
+		'eval'                     => array('tl_class'=>'w50'),
+		'sql'                      => "varchar(12) NOT NULL default '#ff00ff'"
+    ),
+    'bar_border_skipped'                => array(
+        'label'                    => &$GLOBALS['TL_LANG']['tl_content']['bar_border_skipped'],
+        'inputType'                => 'select',
+		'options'                  => array('start' => 'Start', 'end' => 'End', 'middle' => 'Middle', 'bottom' => 'Bottom', 'left' => 'Left', 'top' => 'Top', 'right' => 'Right', 'false' => 'False'),
+		'eval'                     => array('mandatory'=>true, 'tl_class'=>'w50'),
+		'sql'                      => "varchar(32) NOT NULL default 'start'"
+    ),
+    'bar_border_radius'        => array(
+        'label'                    => &$GLOBALS['TL_LANG']['tl_content']['bar_border_radius'],
+        'inputType'                => 'text',
+		'eval'                     => array('tl_class'=>'w50'),
+		'sql'                      => "varchar(12) NOT NULL default '0'"
+    ),
+    'bar_inflate_amount'        => array(
+        'label'                    => &$GLOBALS['TL_LANG']['tl_content']['bar_inflate_amount'],
+        'inputType'                => 'text',
+		'eval'                     => array('tl_class'=>'w50'),
+		'sql'                      => "varchar(12) NOT NULL default 'auto'"
+    ),
+    'bar_point_style'                => array(
+        'label'                    => &$GLOBALS['TL_LANG']['tl_content']['bar_point_style'],
+        'inputType'                => 'select',
+		'options'                  => array('circle' => 'Circle', 'cross' => 'Cross', 'crossRot' => 'Cross Rot', 'dash' => 'Dash', 'line' => 'Line', 'rect' => 'Rect', 'rectRounded' => 'Rect Rounded', 'rectRot' => 'Rect Rot', 'star' => 'Star', 'triangle' => 'Triangle', 'false' => 'False'),
+		'eval'                     => array('mandatory'=>true, 'tl_class'=>'w50'),
+		'sql'                      => "varchar(32) NOT NULL default 'circle'"
+    ),
 
 
-	/**
-	 * Generate the content element
-	 */
-	public function compile()
-	{
-        // Run the initial compile function just to be cool like that
-        //parent::compile();
-	}
 
-    /**
-	 * Initialize the object
-	 *
-	 * @param ContentModel $objElement
-	 * @param string       $strColumn
-	 */
-	public function __construct($objElement, $strColumn='main')
-	{
-	    // Run the original construct function
-        parent::__construct($objElement, $strColumn='main');
-        
-        
-        
-        //echo "<pre>";
-        //print_r(unserialize($this->line_background_colors));
-        //echo "</pre>";
-        
-        
-        
-        
-        // Assemble our table data into usable formats
-        $rows = \StringUtil::deserialize($this->tableitems, true);
-        
-        if($rows != null) {
-        
-            // Assemble our label data as a string
-            $labels = '';
-            for ($x = 1; $x < count($rows[0]); $x++) {
-                $labels .= '"'.$rows[0][$x].'"';
-                if($x != count($rows[0])-1) { $labels .= ', '; }
-            }
-        
-            // Assemble our datasets
-            $datasets = array();
-            foreach($rows as $index=>$row) {
-                
-                $datasets[$index]['label'] = $row[0];
-                $datasets[$index]['data_string'] = '';
-                
-                for($x = 1; $x < count($row); $x++) {
+
+
+    'line_background_colors'       => array(
+        'label'     => &$GLOBALS['TL_LANG']['tl_content']['background_colors'],
+        'exclude'   => true,
+        'inputType' => 'multiColumnWizard',
+        'eval'      => [
+            'columnFields' => [
+                'bg_r' => [
+                    'label'     => &$GLOBALS['TL_LANG']['tl_content']['bg_r'],
+                    'exclude'   => true,
+                    'inputType' => 'text',
+                ],
+                'bg_g' => [
+                    'label'     => &$GLOBALS['TL_LANG']['tl_content']['bg_b'],
+                    'exclude'   => true,
+                    'inputType' => 'text',
+                ],
+                'bg_b' => [
+                    'label'     => &$GLOBALS['TL_LANG']['tl_content']['bg_b'],
+                    'exclude'   => true,
+                    'inputType' => 'text',
+                ],
+                'bg_a' => [
+                    'label'     => &$GLOBALS['TL_LANG']['tl_content']['bg_a'],
+                    'exclude'   => true,
+                    'inputType' => 'text',
+                ],
+            ],
+        ],
+        'sql'       => 'blob NULL',
+    ),
+
+
+
+
     
-                    $datasets[$index]['data'] .= '"' . $row[$x] . '"';
-                    if($x != count($row)-1) { $datasets[$index]['data'] .= ', '; }
-                    
-                }
-                
-                $datasets[$index]['dataset'] = "
-                {
-                    label: '".$datasets[$index]['label']."',
-                    data: [".$datasets[$index]['data']."],
-                },
-                ";
-                
-            }
-            
-            // Include Chart.js and our configuration script
-            $GLOBALS['TL_JAVASCRIPT']['chart_cdn'] = 'https://cdn.jsdelivr.net/npm/chart.js';
-    
-            // Start our config script
-            $config = '
-                document.addEventListener("DOMContentLoaded", function () {
-                    const ctx_'.$this->id.' = document.getElementById("chart_'.$this->id.'")
-                    const line_chart__'.$this->id.' = new Chart(ctx_'.$this->id.', {
-                        type: "line",
-                        data: {
-                            labels: ['.$labels.'],
-                            datasets: [';
-    
-            // Add in our datasets
-            foreach($datasets as $index=>$dataset) {
-                if($index > 0)
-                    $config .= $dataset['dataset'];
-            }
-    
-            // End our config script
-            $config .=  '
-                            ],
-                        },
-                        options: {';
+);
 
-            if($this->animate == 'yes') {
-
-                $config .= '
-                            transitions: {
-                              show: {
-                                animations: {
-                                  x: {
-                                    from: 0
-                                  },
-                                  y: {
-                                    from: 0
-                                  }
-                                }
-                              },
-                              hide: {
-                                animations: {
-                                  x: {
-                                    to: 0
-                                  },
-                                  y: {
-                                    to: 0
-                                  }
-                                }
-                              }
-                            },
-                ';
-            }
-
-
-            if('yes' == 'yes') {
-                
-                
-                $bg_color_array = unserialize($this->line_background_colors);
-                $bg_colors = '';
-                for($x = 0; $x < count($bg_color_array); $x++) {
-                    
-                    $bg_colors .= "'rgba(".$bg_color_array[$x]['bg_r'].", ".$bg_color_array[$x]['bg_g'].", ".$bg_color_array[$x]['bg_b'].", ".$bg_color_array[$x]['bg_a'].")'";
-                    if($x != (count($bg_color_array) -1))
-                        $bg_colors .= ', ';
-                }
-
-                $config .= "
-                            elements: {
-                              line: {
-                                    tension: ".$this->line_tension.",
-                                    borderColor: [".$bg_colors."],
-                                    borderWidth: ".$this->line_border_width.",
-                                    borderDash: ".$this->line_border_dash.",
-                                    borderJointStyle: '".$this->line_border_joint_style."',
-                                    backgroundColor: [".$bg_colors."],
-                                    fill: ".$this->line_fill.",
-                                    
-                                    
-                                },
-                                point: {
-                                    radius: 10,
-                                    pointStyle: 'circle',
-                                    backgroundColor: '#ffffff',
-                                    borderWidth: 5,
-                                    borderColor: '#000000',
-                                }
-                              },
-                ";
-            }
-            
-            
-            $config .= '   scales: {
-                                y: {
-                                    beginAtZero: false,
-                                },
-                            },
-                            plugins: {
-                              legend: {
-                                position: "top",
-                              },
-                              title: {
-                                display: true,
-                                text: "'.$datasets[0]['label'].'"
-                              }
-                            }
-                        },
-                    });
-                });
-            ';
-    
-            // Add our config script to the bottom of the <body> tag
-            $GLOBALS['TL_BODY'][] = '<script>' . $config . '</script>';
-        }
-        
-	}   
-}
-
-class_alias(ContentLineChart::class, 'ContentLineChart');
+$dc['fields'] = array_merge($dc['fields'], $arrFields);
