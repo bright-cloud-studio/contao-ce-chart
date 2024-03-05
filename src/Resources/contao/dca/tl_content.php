1afcae9cbc43c877236b4ff2f95d0eaa8064ba1b
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2024-03-05 08:26:33 -0500

    Update tl_content.php

--- src/Resources/contao/dca/tl_content.php
+++ src/Resources/contao/dca/tl_content.php
@@ -10,7 +10,7 @@
 
 // Get our default 'tl_content' DCA
 $dc = &$GLOBALS['TL_DCA']['tl_content'];
-$GLOBALS['TL_DCA']['tl_content']['palettes']['chart_line'] = '{type_legend},type,headline;{dataset_legend},tableitems;{chart_line_config_legend},animate,line_tension,line_border_color,line_border_width,line_border_dash,line_border_joint_style,line_background_color,line_fill;{line_colors_legend},line_background_colors;{template_legend:hide},customTpl;{protected_legend:hide},protected;{expert_legend:hide},guests,cssID;{invisible_legend:hide},invisible,start,stop';
+$GLOBALS['TL_DCA']['tl_content']['palettes']['chart_line'] = '{type_legend},type,headline;{dataset_legend},tableitems;{chart_line_config_legend},animate,line_tension,line_border_color,line_border_width,line_border_dash,line_border_joint_style,line_background_color,line_fill;{line_border_colors_legend},line_border_colors;{line_background_colors_legend},line_background_colors;{template_legend:hide},customTpl;{protected_legend:hide},protected;{expert_legend:hide},guests,cssID;{invisible_legend:hide},invisible,start,stop';
 $GLOBALS['TL_DCA']['tl_content']['palettes']['chart_bar'] = '{type_legend},type,headline;{dataset_legend},tableitems;{chart_bar_config_legend},bar_background_color,bar_border_width,bar_border_color,bar_border_skipped,bar_border_radius,bar_inflate_amount,bar_point_style;{template_legend:hide},customTpl;{protected_legend:hide},protected;{expert_legend:hide},guests,cssID;{invisible_legend:hide},invisible,start,stop';
 
 $arrFields = array(
@@ -117,7 +117,37 @@ $arrFields = array(
 
 
     'line_background_colors'       => array(
-        'label'     => &$GLOBALS['TL_LANG']['tl_content']['background_colors'],
+        'label'     => &$GLOBALS['TL_LANG']['tl_content']['line_background_colors'],
+        'exclude'   => true,
+        'inputType' => 'multiColumnWizard',
+        'eval'      => [
+            'columnFields' => [
+                'bg_r' => [
+                    'label'     => &$GLOBALS['TL_LANG']['tl_content']['bg_r'],
+                    'exclude'   => true,
+                    'inputType' => 'text',
+                ],
+                'bg_g' => [
+                    'label'     => &$GLOBALS['TL_LANG']['tl_content']['bg_b'],
+                    'exclude'   => true,
+                    'inputType' => 'text',
+                ],
+                'bg_b' => [
+                    'label'     => &$GLOBALS['TL_LANG']['tl_content']['bg_b'],
+                    'exclude'   => true,
+                    'inputType' => 'text',
+                ],
+                'bg_a' => [
+                    'label'     => &$GLOBALS['TL_LANG']['tl_content']['bg_a'],
+                    'exclude'   => true,
+                    'inputType' => 'text',
+                ],
+            ],
+        ],
+        'sql'       => 'blob NULL',
+    ),
+    'line_border_colors'       => array(
+        'label'     => &$GLOBALS['TL_LANG']['tl_content']['line_border_colors'],
         'exclude'   => true,
         'inputType' => 'multiColumnWizard',
         'eval'      => [
