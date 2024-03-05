44ad6b852c9f042fa74192bf52c0e234aff9488f
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2024-03-05 09:32:40 -0500

    Update tl_content.php

--- src/Resources/contao/languages/en/tl_content.php
+++ src/Resources/contao/languages/en/tl_content.php
@@ -16,11 +16,9 @@ $GLOBALS['TL_LANG']['tl_content']['chart_line_config_legend']   = 'Chart - Line 
 /* Fields */
 $GLOBALS['TL_LANG']['tl_content']['animate']          = array('Show/Hide Animation', 'Choose if the lines should animate when showing or hiding');
 $GLOBALS['TL_LANG']['tl_content']['line_tension']          = array('Line Tension', 'Lower tension makes lines rigid, higher makes them smoother');
-$GLOBALS['TL_LANG']['tl_content']['line_border_color']          = array('Line Stroke Color', 'HTML color for the stroke color of the lines');
 $GLOBALS['TL_LANG']['tl_content']['line_border_width']          = array('Line Stroke Width', 'Changes the thickness of the line stroke');
 $GLOBALS['TL_LANG']['tl_content']['line_border_dash']          = array('Line Border Dash', 'First number is the width of the dash, second number is the space between the dashes');
 $GLOBALS['TL_LANG']['tl_content']['line_border_joint_style']          = array('Line Stroke Joint Style', 'Choose from different styles for the joint styles');
-$GLOBALS['TL_LANG']['tl_content']['line_background_color']          = array('Line Background Color', 'HTML color for the line background');
 $GLOBALS['TL_LANG']['tl_content']['line_fill']          = array('Line Fill', 'Choose if the lines should fill the background to the bottom of the chart');
 
 
@@ -31,3 +29,24 @@ $GLOBALS['TL_LANG']['tl_content']['bar_border_skipped']          = array('Bar St
 $GLOBALS['TL_LANG']['tl_content']['bar_border_radius']          = array('Bar Stroke Radius', 'Enter the bar radius');
 $GLOBALS['TL_LANG']['tl_content']['bar_inflate_amount']          = array('Bar Inflate Amount', 'Enter how much the bars should inflate');
 $GLOBALS['TL_LANG']['tl_content']['bar_point_style']          = array('Point Style', 'Select the point style for the bar, or if there should be none at all');
+
+$GLOBALS['TL_LANG']['tl_content']['line_border_colors']          = array('Line Border Colors', 'Enter the RGBA values for the invidivudal line border colors');
+$GLOBALS['TL_LANG']['tl_content']['line_border_colors']['bd_r']          = array('Red', 'Red Value');
+$GLOBALS['TL_LANG']['tl_content']['line_border_colors']['bd_g']          = array('Green', 'Green Value');
+$GLOBALS['TL_LANG']['tl_content']['line_border_colors']['bd_b']          = array('Blue', 'Blue Value');
+$GLOBALS['TL_LANG']['tl_content']['line_border_colors']['bd_a']          = array('Alpha', 'Alpha Value');
+
+$GLOBALS['TL_LANG']['tl_content']['line_background_colors']          = array('Line Background Colors', 'Enter the RGBA values for the invidivudal line background colors');
+$GLOBALS['TL_LANG']['tl_content']['line_border_colors']['bg_r']          = array('Red', 'Red Value');
+$GLOBALS['TL_LANG']['tl_content']['line_border_colors']['bg_g']          = array('Green', 'Green Value');
+$GLOBALS['TL_LANG']['tl_content']['line_border_colors']['bg_b']          = array('Blue', 'Blue Value');
+$GLOBALS['TL_LANG']['tl_content']['line_border_colors']['bg_a']          = array('Alpha', 'Alpha Value');
+
+$GLOBALS['TL_LANG']['tl_content']['line_point_radius']          = array('Point Radius', 'Enter the radius value for the line points');
+$GLOBALS['TL_LANG']['tl_content']['line_point_style']          = array('Point Style', 'Select the style the line points should use');
+$GLOBALS['TL_LANG']['tl_content']['line_point_background_color']      = array('Point Background Color', 'Enter an HTML color for the point background');
+$GLOBALS['TL_LANG']['tl_content']['line_point_border_width']          = array('Point Border Width', 'Enter the point border width');
+$GLOBALS['TL_LANG']['tl_content']['line_point_border_color']          = array('Point Border Color', 'Enter an HTML color for the point border color');
+
+
+
