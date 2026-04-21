b4630fa7dd4fb213612ccce98d94d38b9728f372
Bright Cloud Studio <mark@brightcloudstudio.com>
2026-04-21 13:00:51 -0400

    Update config.php

--- src/Resources/contao/config/config.php
+++ src/Resources/contao/config/config.php
@@ -1,17 +1,17 @@
 <?php
-
     /**
-    * @copyright  Bright Cliud Studio
+    * @copyright  Bright Cloud Studio
     * @author     Bright Cloud Studio
     * @package    Contao CE Chart
     * @license    LGPL-3.0+
     * @see	       https://github.com/bright-cloud-studio/contao-ce-chart
     */
-    
-    array_insert($GLOBALS['TL_CTE']['datasets'], 10, array
+
+    use Contao\ArrayUtil;
+
+    ArrayUtil::arrayInsert($GLOBALS['TL_CTE']['datasets'], 10, array
     (
         'chart_line' => 'Bcs\ChartBundle\ContentLineChart',
         'chart_bar' => 'Bcs\ChartBundle\ContentBarChart'
     ));
-
 ?>
